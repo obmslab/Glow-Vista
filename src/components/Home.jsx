ce099bc07e105d046a26b9dd8f0d97f6a37c3ec8
obmslab <banothe@nitrkl.ac.in>
2024-05-24 12:56:04 +0530

    break

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         </div>
         <div id="meta_data">
           <div id="notice">
-            <p><marquee direction="">NOTICE 1:- <a href="https://forms.gle/V3DBC39MtqNHfUVt8" target="_blank">Internship Openings</a> Interested People can apply for internsip in glow vista. | NOTICE 2:- <a href="https://forms.gle/V3DBC39MtqNHfUVt8" target="_blank"> Job  Openings</a> Interested People can apply for Job in glow vista.</marquee></p>
+            <p><marquee direction="">NOTICE 1:- <a href="https://forms.gle/V3DBC39MtqNHfUVt8" target="_blank">Internship Openings</a> Interested People can apply for internsip in glow vista. <br /> NOTICE 2:- <a href="https://forms.gle/V3DBC39MtqNHfUVt8" target="_blank"> Job  Openings</a> Interested People can apply for Job in glow vista.</marquee></p>
             
           </div>
         </div>
